371cb04f5e50b6abef43750cc18959207f29267a
sammyhak <samsonbaruwa6@gmail.com>
2024-03-06 02:59:54 +0100

    chores

--- src/components/featuredPro.vue
+++ src/components/featuredPro.vue
@@ -4,8 +4,8 @@
       <div class="header">
         <h3>Featured Products</h3>
         <div class="controls">
-          <img src="../assets/previous.svg" alt="previous">
-          <img src="../assets/next.svg" alt="next">
+          <img src="../assets/previous.svg" alt="previous" class="previous">
+          <img src="../assets/next.svg" alt="next" class="next">
         </div>
       </div>
 
@@ -19,7 +19,7 @@
             <div class="price">N2000</div>
           </div>
           <div class="description">
-            Pain relief
+            Pain relief 1
           </div>
         </div>
 
@@ -32,7 +32,7 @@
             <div class="price">N2000</div>
           </div>
           <div class="description">
-            Pain relief
+            Pain relief 2
           </div>
         </div>
 
@@ -45,7 +45,7 @@
             <div class="price">N2000</div>
           </div>
           <div class="description">
-            Pain relief
+            Pain relief 3
           </div>
         </div>
 
@@ -58,13 +58,79 @@
             <div class="price">N2000</div>
           </div>
           <div class="description">
-            Pain relief
+            Pain relief 4
+          </div>
+        </div>
+
+        <div class="featured-product">
+          <div class="img-container">
+            <img src="../assets/para.png" alt="drug">
+          </div>
+          <div class="title">
+            Paracetamol
+            <div class="price">N2000</div>
+          </div>
+          <div class="description">
+            Pain relief 5
+          </div>
+        </div>
+
+        <div class="featured-product">
+          <div class="img-container">
+            <img src="../assets/para.png" alt="drug">
+          </div>
+          <div class="title">
+            Paracetamol
+            <div class="price">N2000</div>
+          </div>
+          <div class="description">
+            Pain relief 6
+          </div>
+        </div>
+
+        <div class="featured-product">
+          <div class="img-container">
+            <img src="../assets/para.png" alt="drug">
+          </div>
+          <div class="title">
+            Paracetamol
+            <div class="price">N2000</div>
+          </div>
+          <div class="description">
+            Pain relief 7
+          </div>
+        </div>
+
+        <div class="featured-product">
+          <div class="img-container">
+            <img src="../assets/para.png" alt="drug">
+          </div>
+          <div class="title">
+            Paracetamol
+            <div class="price">N2000</div>
+          </div>
+          <div class="description">
+            Pain relief 8
+          </div>
+        </div>
+
+        <div class="featured-product">
+          <div class="img-container">
+            <img src="../assets/para.png" alt="drug">
+          </div>
+          <div class="title">
+            Paracetamol
+            <div class="price">N2000</div>
+          </div>
+          <div class="description">
+            Pain relief 9
           </div>
         </div>
       </div>
     </div>
   </section>
 </template>
+
 
 <script>
 import '../styles/components/featuredPro.scss';
@@ -74,55 +140,27 @@ export default {
 
 }
 
+document.addEventListener("DOMContentLoaded", function () {
+  const previous = document.querySelector(".previous");
+  const next = document.querySelector(".next");
+  const slider = document.querySelector('.products');
+
+  slider.scrollLeft = 0;
+  previous.addEventListener("click", function () {
+    moveSlide(-1);
+  });
+
+  next.addEventListener("click", function () {
+    moveSlide(1);
+  });
+});
+
+function moveSlide(direction) {  
+  const slider = document.querySelector('.products');
+  if(direction == -1){        
+    slider.scrollLeft -= 1290;
+  }else{
+    slider.scrollLeft += 1290;    
+  }  
+}
 </script>
-
-<style>
-.font-for-inter {
-  font-family: facee;
-}
-
-.fea {
-
-  color: #258576;
-}
-
-.card {
-  padding: 0;
-
-}
-
-.card-wrapper {}
-
-.card {
-  border: none !important;
-}
-
-.bu {
-  /* color:green !important; */
-  width: 50px !important;
-  height: 50px !important;
-}
-
-.firstbu {
-  position: absolute !important;
-  top: -20px !important;
-  left: 590px !important;
-  width: 40px !important;
-  height: 40px !important;
-
-}
-
-.firsttwo {
-  position: absolute !important;
-  top: -40px !important;
-  left: 0px !important;
-  width: 40px !important;
-  height: 40px !important;
-
-}
-
-.carousel-dark .carousel-control-prev-icon,
-.carousel-dark .carousel-control-next-icon {
-  filter: invert(1) grayscale(100) hue-rotate(120deg);
-}
-</style>
